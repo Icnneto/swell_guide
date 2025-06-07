594b5a5796188502e98385abc9cb3ce870315a0d
Israel Nunes <israelnetonunes@gmail.com>
2025-06-07 17:52:11 -0300

    feat: add html inside e-mail template - converte external css to inline

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 from src.services.llm_service import LLMService
 from src.services.weather_service import WeatherService
 from src.utils.markdown_to_html_service import markdown_to_html
+from src.utils.html_to_template import insert_html_template
 from dotenv import load_dotenv
+from pathlib import Path
+from premailer import transform
 
 load_dotenv()
-
 
 STORMGLASS_API_KEY = os.getenv("STORMGLASS_API_KEY")
 if not STORMGLASS_API_KEY:
@@ -14,6 +16,8 @@ if not STORMGLASS_API_KEY:
 surf_spot = "Florianópolis, SC - Brasil"
 floripa_latitude = -27.593500
 floripa_longitude = -48.558540
+
+PATH_TO_TEMPLATE = Path("templates") / "email_template.html"
 
 # def main():
 
@@ -59,15 +63,34 @@ floripa_longitude = -48.558540
 
 
 # weather_service retorna dict da API
-weather_service = WeatherService(STORMGLASS_API_KEY)
-print('Resgatando dados meteorológicos...')
-weather_data = weather_service.fetch_weather_data(floripa_latitude, floripa_longitude)
+# weather_service = WeatherService(STORMGLASS_API_KEY)
+# print('Resgatando dados meteorológicos...')
+# weather_data = weather_service.fetch_weather_data(floripa_latitude, floripa_longitude)
 
 # llm_service recebe dict e retorna markdown
-llm_service = LLMService()
-print('Chamando análise da LLM...')
-llm_response = llm_service.call_ai_analysis(weather_data, surf_spot)
-print('Gerando resposta da IA:')
+# llm_service = LLMService()
+# print('Chamando análise da LLM...')
+# llm_response = llm_service.call_ai_analysis(weather_data, surf_spot)
+# print('Gerando resposta da IA:')
+
+with open("llm_output_test.md", "r", encoding="utf-8") as md:
+    markdown = md.read()
+
 
 # transformar markdown em html
-html_output = markdown_to_html(llm_response)
+html_output = markdown_to_html(markdown)
+
+with open('result.html', 'w', encoding="utf-8") as output_file:
+    output_file.write(html_output)
+
+
+# inserir html no template de e-mail
+embedded_html = insert_html_template(PATH_TO_TEMPLATE, html_output)
+
+with open('result2.html', 'w', encoding="utf-8") as output_file:
+    output_file.write(embedded_html)
+
+final_html = transform(embedded_html)
+
+with open('final_result.html', 'w', encoding="utf-8") as output_file:
+    output_file.write(final_html)
